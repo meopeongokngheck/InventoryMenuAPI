46ed98620882464165ad7461a7729d553a3cf2ff
korado531m7 <support.system@icloud.com>
2018-10-30 15:23:38 +0900

    Supported EnderChest and Enchanting Table and bug fix
    In version 1.1.0, supported EnderChest and Enchantment Table.
    Verifying the maximum key in item array, if not collect, it will be
    thrown the error.
    double chest inventory bug fixed.

--- src/korado531m7/InventoryMenuAPI/InventoryMenuAPI.php
+++ src/korado531m7/InventoryMenuAPI/InventoryMenuAPI.php
@@ -3,12 +3,14 @@ namespace korado531m7\InventoryMenuAPI;
 
 use korado531m7\InventoryMenuAPI\event\InventoryMenuCloseEvent;
 use korado531m7\InventoryMenuAPI\event\InventoryMenuGenerateEvent;
-use korado531m7\task\DelayAddWindowTask;
+use korado531m7\InventoryMenuAPI\task\DelayAddWindowTask;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\block\BlockFactory;
 use pocketmine\block\BlockIds;
 use pocketmine\item\Item;
+use pocketmine\inventory\EnchantInventory;
+use pocketmine\level\Position;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\NetworkLittleEndianNBTStream;
 use pocketmine\nbt\tag\CompoundTag;
@@ -17,6 +19,8 @@ use pocketmine\network\mcpe\protocol\UpdateBlockPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\tile\Chest as TileChest;
 use pocketmine\tile\Furnace as TileFurnace;
+use pocketmine\tile\EnchantTable as TileEnchantTable;
+use pocketmine\tile\EnderChest as TileEnderChest;
 use pocketmine\tile\Tile;
 
 class InventoryMenuAPI extends PluginBase{
@@ -27,6 +31,8 @@ class InventoryMenuAPI extends PluginBase{
     const INVENTORY_TYPE_CHEST = 1;
     const INVENTORY_TYPE_DOUBLE_CHEST = 2;
     const INVENTORY_TYPE_FURNACE = 3;
+    const INVENTORY_TYPE_ENCHANTING_TABLE = 4;
+    const INVENTORY_TYPE_ENDER_CHEST = 5;
     
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
@@ -48,29 +54,58 @@ class InventoryMenuAPI extends PluginBase{
         $y = ((int)$player->y + 4);
         $z = ((int)$player->z + mt_rand(-1,1));
         if($player->getLevel()->getTileAt($x,$y,$z) !== null) $y = ((int)$player->y + 3);
-        
+        if(count($items) === 0) $maxKey = 0;
+        else $maxKey = max(array_keys($items));
         switch($inventoryType){
             case self::INVENTORY_TYPE_FURNACE:
+                if($maxKey > 2) throw new \RuntimeException('Invalid key for furnace expected 0, 1 got '.$maxKey);
                 self::sendFakeBlock($player,$x,$y,$z,BlockIds::FURNACE);
                 $nbt = TileFurnace::createNBT(new Vector3($x,$y,$z), 0, Item::get(0,0), $player);
                 $nbt->setString('CustomName',$inventoryName);
                 $tile = Tile::createTile(Tile::FURNACE, $player->getLevel(), $nbt);
-				$tag = new CompoundTag();
-				$tag->setString('id', $tile->getSaveId());
-				$tag->setString('CustomName', $inventoryName);
+                $tag = new CompoundTag();
+                $tag->setString('id', $tile->getSaveId());
+                $tag->setString('CustomName', $inventoryName);
+                $inv = $tile->getInventory();
+            break;
+            
+            case self::INVENTORY_TYPE_ENCHANTING_TABLE:    
+                if($maxKey > 2) throw new \RuntimeException('Invalid key for furnace expected 0, 1 got '.$maxKey);
+                self::sendFakeBlock($player,$x,$y,$z,BlockIds::ENCHANTING_TABLE);
+                $nbt = TileEnchantTable::createNBT(new Vector3($x,$y,$z), 0, Item::get(0,0), $player);
+                $nbt->setString('CustomName',$inventoryName);
+                $tile = Tile::createTile(Tile::ENCHANT_TABLE, $player->getLevel(), $nbt);
+                $tag = new CompoundTag();
+                $tag->setString('id', $tile->getSaveId());
+                $tag->setString('CustomName', $inventoryName);
+                $inv = new EnchantInventory(new Position($x,$y,$z,$player->getLevel()));
+            break;
+            
+            case self::INVENTORY_TYPE_ENDER_CHEST:    
+                if($maxKey > 27) throw new \RuntimeException('Invalid key for furnace expected between 0 and 26 got '.$maxKey);
+                self::sendFakeBlock($player,$x,$y,$z,BlockIds::ENDER_CHEST);
+                $nbt = TileEnchantTable::createNBT(new Vector3($x,$y,$z), 0, Item::get(0,0), $player);
+                $nbt->setString('CustomName',$inventoryName);
+                $tile = Tile::createTile(Tile::ENDER_CHEST, $player->getLevel(), $nbt);
+                $player->getEnderChestInventory()->setHolderPosition($tile);
+                $tag = new CompoundTag();
+                $tag->setString('id', $tile->getSaveId());
+                $tag->setString('CustomName', $inventoryName);
+                $inv = $player->getEnderChestInventory();
             break;
             
             case self::INVENTORY_TYPE_DOUBLE_CHEST:
+                if($maxKey > 54) throw new \RuntimeException('Invalid key for furnace expected between 0 and 53 got '.$maxKey);
                 self::sendFakeBlock($player,$x,$y,$z + 1,BlockIds::CHEST);
                 $nbt2 = TileChest::createNBT(new Vector3($x,$y,$z + 1), 0, Item::get(0,0), $player);
                 $tile2 = Tile::createTile(Tile::CHEST, $player->getLevel(), $nbt2);
                 $writer = new NetworkLittleEndianNBTStream();
-				$tag = new CompoundTag();
-				$tag->setString('id', $tile->getSaveId());
-				$tag->setInt('pairx', $x);
-				$tag->setInt('pairz', $z);
-				$tag->setString('CustomName', $inventoryName);
-				
+                $tag = new CompoundTag();
+                $tag->setString('id', $tile2->getSaveId());
+                $tag->setInt('pairx', $x);
+                $tag->setInt('pairz', $z);
+                $tag->setString('CustomName', $inventoryName);
+                
                 $pk = new BlockEntityDataPacket;
                 $pk->x = $x;
                 $pk->y = $y;
@@ -78,14 +113,16 @@ class InventoryMenuAPI extends PluginBase{
                 $pk->namedtag = $writer->write($tag);
                 $player->dataPacket($pk);
             case self::INVENTORY_TYPE_CHEST:
+                if($maxKey > 27) throw new \RuntimeException('Invalid key for furnace expected between 0 and 26 got '.$maxKey);
                 self::sendFakeBlock($player,$x,$y,$z,BlockIds::CHEST);
                 $nbt = TileChest::createNBT(new Vector3($x,$y,$z), 0, Item::get(0,0), $player);
                 $nbt->setString('CustomName',$inventoryName);
                 $tile = Tile::createTile(Tile::CHEST, $player->getLevel(), $nbt);
                 $tag = new CompoundTag();
-				$tag->setString('id', $tile->getSaveId());
-				$tag->setString('CustomName', $inventoryName);
+                $tag->setString('id', $tile->getSaveId());
+                $tag->setString('CustomName', $inventoryName);
                 if($inventoryType == self::INVENTORY_TYPE_DOUBLE_CHEST) $tile->pairWith($tile2);
+                $inv = $tile->getInventory();
             break;
         }
         
@@ -97,13 +134,18 @@ class InventoryMenuAPI extends PluginBase{
         $pk->namedtag = $writer->write($tag);
         $player->dataPacket($pk);
         
-        $inv = $tile->getInventory();
         foreach($items as $itemkey => $item){
             $inv->setItem($itemkey,$item);
         }
-		Server::getInstance()->getPluginManager()->callEvent(new InventoryMenuGenerateEvent($player,$items,$tile,$inventoryType));
+        Server::getInstance()->getPluginManager()->callEvent(new InventoryMenuGenerateEvent($player,$items,$tile,$inventoryType));
         self::saveInventory($player);
         switch($inventoryType){
+            case self::INVENTORY_TYPE_ENDER_CHEST:
+            case self::INVENTORY_TYPE_ENCHANTING_TABLE:
+                self::$inventoryMenuVar[$player->getName()] = array($inventoryType,$tile->getSaveId(),$x,$y,$z,$player->getLevel()->getName(),$isCloseType,$inv);
+                $player->addWindow($inv);
+            break;
+            
             case self::INVENTORY_TYPE_FURNACE:
             case self::INVENTORY_TYPE_CHEST:
                 self::$inventoryMenuVar[$player->getName()] = array($inventoryType,$tile->getSaveId(),$x,$y,$z,$player->getLevel()->getName(),$isCloseType);
@@ -169,6 +211,12 @@ class InventoryMenuAPI extends PluginBase{
             $level = Server::getInstance()->getLevelByName($data[5]);
             Server::getInstance()->getPluginManager()->callEvent(new InventoryMenuCloseEvent($player, $level->getTile(new Vector3($data[2],$data[3],$data[4]))));
             switch($data[0]){
+                case self::INVENTORY_TYPE_ENCHANTING_TABLE:
+                case self::INVENTORY_TYPE_ENDER_CHEST:
+                    $player->removeWindow($data[7]);
+                    self::sendFakeBlock($player,$data[2],$data[3],$data[4],BlockIds::AIR);
+                break;
+                
                 case self::INVENTORY_TYPE_DOUBLE_CHEST:
                     self::sendFakeBlock($player,$data[2],$data[3],$data[4] + 1,BlockIds::AIR);
                     $level->removeTile($level->getTile(new Vector3($data[2],$data[3],$data[4] + 1)));
@@ -198,7 +246,7 @@ class InventoryMenuAPI extends PluginBase{
      * @return array
      */
     public static function getData(Player $player) : array{
-        return self::$inventoryMenuVar[$player->getName()] ?? array();
+        return self::$inventoryMenuVar[$player->getName()] ?? [];
     }
     
     public static function saveInventory(Player $player){
@@ -225,7 +273,7 @@ class InventoryMenuAPI extends PluginBase{
         $pk->x = $x;
         $pk->y = $y;
         $pk->z = $z;
-		$pk->flags = UpdateBlockPacket::FLAG_ALL;
+        $pk->flags = UpdateBlockPacket::FLAG_ALL;
         $pk->blockRuntimeId = BlockFactory::toStaticRuntimeId($blockid);
         $player->dataPacket($pk);
     }
